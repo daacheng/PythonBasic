63fcacd3e1af7d6f17216d5ab5b9a915e8f895d9
daacheng <1193685537@qq.com>
2018-01-28 23:19:55 +0800

    通过关键词，爬取今日头条新闻，并保存csv文件 Signed-off-by: daacheng <1193685537@qq.com>

--- pythonForTouTiaoNews.py
+++ pythonForTouTiaoNews.py
@@ -3,7 +3,6 @@ import requests
 import json
 import csv
 
-
 # 获取爬取到的数据，并保存成csv格式文件
 def get_data(url):
     res = requests.get(url)
@@ -28,14 +27,12 @@ def get_data(url):
             # print('标题：',title,'  评论数:',comment_count,'   文章链接：',article_url)
         if 'keywords' in news:
             keywords = news['keywords']
-        line = title + '|' + article_url + '|' + keywords + '|' + str(comment_count)
         content = [title, article_url, keywords, str(comment_count)]
         # print(content)
         with open('toutiao_news.csv', 'a', newline='', encoding='utf-8') as f:
             f_csv = csv.writer(f)
             f_csv.writerow(content)
             f.close()
-
 
 pkeywords = '''阿里巴巴
 人工智能
