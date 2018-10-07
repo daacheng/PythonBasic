fc63e396bdbd84be15501311dfbe725b58bee5f4
daacheng <1193685537@qq.com>
2018-10-07 17:50:31 +0800

    Delete perfectSquare.py

--- perfectSquare.py
+++
@@ -1,9 +0,0 @@
-import math
-for i in range(1,10001):
-	i1=i+100
-	i2=i+268
-	if (math.sqrt(i1)%1==0) and (math.sqrt(i2)%1==0):
-		print(i)
-		print(i1,':',math.sqrt(i1))
-		print(i2,':',math.sqrt(i2))
-		print('####################')
