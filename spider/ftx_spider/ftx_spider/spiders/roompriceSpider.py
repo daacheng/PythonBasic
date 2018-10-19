1a35ed31366a8cee9193fb25144add1fad284756
dacheng <1193685537@qq.com>
2018-10-19 17:03:38 +0800

    利用scrapy爬取房天下二手房数据 Signed-off-by: dacheng <1193685537@qq.com>

--- spider/ftx_spider/ftx_spider/spiders/roompriceSpider.py
+++ spider/ftx_spider/ftx_spider/spiders/roompriceSpider.py
@@ -1,7 +1,9 @@
 import scrapy
 from ftx_spider.items import FtxSpiderItem
 
+
 class roompriceSpider(scrapy.Spider):
+    
     name = 'roomprice'
 
     start_urls = ['http://wuhan.esf.fang.com/house-a013126/i3']
@@ -10,11 +12,12 @@ class roompriceSpider(scrapy.Spider):
         room_domain = FtxSpiderItem()
         rooms = response.xpath('//dl[@class="clearfix"]')
         for room in rooms:
+            # 标题
             title_list = room.xpath('./dd//span[@class="tit_shop"]/text()')
             if len(title_list):
                 title = title_list.extract_first().replace(' ', '').replace('\r\n', '')
                 room_domain['title'] = title
-
+            # 房屋信息(户型，面积，楼层，方向，建房时间)
             info_list = room.xpath('./dd//p[@class="tel_shop"]/text()').extract()
             if len(info_list) > 4:
                 huxing = info_list[0].replace(' ', '').replace('\r\n', '')  # 户型
@@ -28,22 +31,22 @@ class roompriceSpider(scrapy.Spider):
                 room_domain['floor'] = floor
                 room_domain['fangxiang'] = fangxiang
                 room_domain['year'] = year
-
+            # 小区
             shop_community_list = room.xpath('./dd//p[@class="add_shop"]/a/@title').extract()
             if shop_community_list:
                 shop_community = shop_community_list[0]
                 room_domain['shop_community'] = shop_community
-
+            # 地点
             address_list = room.xpath('./dd//p[@class="add_shop"]/span/text()').extract()
             if address_list:
                 address = address_list[0]
                 room_domain['address'] = address
-
+            # 总价
             total_price_list = room.xpath('./dd[@class="price_right"]/span/b/text()').extract()
             if total_price_list:
                 total_price = total_price_list[0]
                 room_domain['total_price'] = total_price
-
+            # 单价
             price_list = room.xpath('./dd[@class="price_right"]/span/text()').extract()
             if price_list:
                 price = price_list[1].replace('�O', 'm2')
