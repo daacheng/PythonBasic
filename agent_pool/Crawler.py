f971414bbe83133c58100e0dea0939a425525199
luzhicheng <luzhicheng@whbaihongsoft.com>
2018-12-03 17:58:53 +0800

    企业信息爬虫 Signed-off-by: luzhicheng <luzhicheng@whbaihongsoft.com>

--- agent_pool/Crawler.py
+++ agent_pool/Crawler.py
@@ -1,6 +1,7 @@
 import requests
 from pyquery import PyQuery as pq
 from bs4 import BeautifulSoup
+import time
 
 """
     爬取代理网站的免费代理并返回
@@ -10,9 +11,10 @@ from bs4 import BeautifulSoup
 class Crawler(object):
 
     def get_crawler_proxy(self):
-        # proxy_list = self.crawl_xici()
-        proxy_list = self.crawl_66()
-        return proxy_list
+        proxy_list_xici = self.crawl_xici()
+        proxy_list_66 = self.crawl_66()
+        proxy_list_kuaidaili = self.crawl_kuaidaili()
+        return proxy_list_xici + proxy_list_66 + proxy_list_kuaidaili
 
     def crawl_xici(self):
         """
@@ -21,26 +23,30 @@ class Crawler(object):
         print('爬取西刺代理......')
         proxy_list = []
         for i in range(1, 20):
+            try:
+                url = 'http://www.xicidaili.com/nn/' + str(i)
+                # url = 'http://www.baidu.com'
+                headers = {
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Accept-Encoding': 'gzip, deflate',
+                    'Accept-Language': 'zh-CN,zh;q=0.9',
+                    'Host': 'www.xicidaili.com',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+                }
 
-            url = 'http://www.xicidaili.com/nn/' + str(i)
-            # url = 'http://www.baidu.com'
-            headers = {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'zh-CN,zh;q=0.9',
-                'Host': 'www.xicidaili.com',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-            }
-
-            res = requests.get(url, headers=headers)
-            doc = pq(res.text)
-            for odd in doc('.odd').items():
-                info_list = odd.find('td').text().split(' ')
-                print(info_list)
-                if len(info_list) == 11:
-                    proxy = info_list[1].strip() + ':' + info_list[2].strip()
-                    print(proxy)
-                    proxy_list.append(proxy)
+                res = requests.get(url, headers=headers)
+                if res.status_code == 200:
+                    doc = pq(res.text)
+                    for odd in doc('.odd').items():
+                        info_list = odd.find('td').text().split(' ')
+                        # print(info_list)
+                        if len(info_list) == 11:
+                            proxy = info_list[1].strip() + ':' + info_list[2].strip()
+                            proxy = proxy.replace(' ', '')
+                            proxy_list.append(proxy)
+            except Exception as e:
+                continue
+        print('爬取到西刺代理 %s 个' % len(proxy_list))
         return proxy_list
 
 
@@ -51,31 +57,69 @@ class Crawler(object):
         print('爬取66代理......')
         proxies = set()
         for i in range(2, 50):
+            try:
+                url = 'http://www.66ip.cn/' + str(i) + '.html'
+                headers = {
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Accept-Encoding': 'gzip, deflate',
+                    'Accept-Language': 'zh-CN,zh;q=0.9',
+                    'Host': 'www.66ip.cn',
+                    'Referer': 'http://www.66ip.cn/',
+                    'Cookie': 'yd_cookie=9ec1ced4-ed0d-4cc6f642d3cad3acd1e747a5d08e8699e047; Hm_lvt_1761fabf3c988e7f04bec51acd4073f4=1543369812,1543391648,1543454297,1543800567; _ydclearance=16fa6a58623529898e43ea54-c25d-4351-890e-4671a1f592b9-1543838015; Hm_lpvt_1761fabf3c988e7f04bec51acd4073f4=1543830819',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36'
+                }
 
-            url = 'http://www.66ip.cn/' + str(i) + '.html'
-            headers = {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Accept-Encoding': 'gzip, deflate',
-                'Accept-Language': 'zh-CN,zh;q=0.9',
-                'Host': 'www.66ip.cn',
-                'Referer': 'http://www.66ip.cn/',
-                'Cookie': 'yd_cookie=845c63d7-6e0d-471381af08539abbfb4b0d1e19c462c47e54; _ydclearance=67ad46765cf76d5ebdc6a049-08e2-4733-a6c4-49fc416e3cb5-1543761306; Hm_lvt_1761fabf3c988e7f04bec51acd4073f4=1542515035,1542515047,1542810896,1543754108; Hm_lpvt_1761fabf3c988e7f04bec51acd4073f4=1543754108',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36'
-            }
+                res = requests.get(url, headers=headers)
+                # print(res.status_code)
+                # print(res.text)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                items = soup.select('.container table tr')
 
-            res = requests.get(url, headers=headers)
-            # print(res.status_code)
-            # print(res.text)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            items = soup.select('.container table tr')
-
-            for i in range(1, len(items)):
-                tds = items[i].select('td')
-                ip = tds[0].string
-                port = tds[1].string
-                proxy = ip + ':' + port
-                if proxy:
-                    # print(proxy)
-                    # print(proxy.replace(' ', ''))
-                    proxies.add(proxy.replace(' ', ''))
+                for i in range(1, len(items)):
+                    tds = items[i].select('td')
+                    ip = tds[0].string
+                    port = tds[1].string
+                    proxy = ip + ':' + port
+                    if proxy:
+                        # print(proxy)
+                        # print(proxy.replace(' ', ''))
+                        proxies.add(proxy.replace(' ', ''))
+            except Exception as e:
+                continue
+        print('爬取到66代理 %s 个' % len(proxies))
         return list(proxies)
+
+
+    def crawl_kuaidaili(self):
+        print('爬取快代理......')
+        proxies = set()
+        for i in range(1, 50):
+            try:
+                url = 'https://www.kuaidaili.com/free/inha/' + str(i)
+                time.sleep(1)
+                headers = {
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Accept-Encoding': 'gzip, deflate',
+                    'Accept-Language': 'zh-CN,zh;q=0.9',
+                    'Host': 'www.kuaidaili.com',
+                    'Referer': 'http://www.66ip.cn/',
+                    'Cookie': 'channelid=0; sid=1543828498576461; Hm_lvt_7ed65b1cc4b810e9fd37959c9bb51b31=1543828501; _ga=GA1.2.2019923346.1543828501; _gid=GA1.2.1278257690.1543828501; _gat=1; Hm_lpvt_7ed65b1cc4b810e9fd37959c9bb51b31=1543828618',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36'
+                }
+
+                res = requests.get(url, headers=headers)
+                # print(res.status_code)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                items = soup.select('#list tbody tr')
+                for item in items:
+                    tds = item.select('td')
+                    ip = tds[0].string
+                    port = tds[1].string
+                    proxy = ip + ':' + port
+                    if proxy:
+                        proxies.add(proxy.replace(' ', ''))
+            except Exception as e:
+                continue
+        print('爬取到块代理 %s 个' % len(proxies))
+        return list(proxies)
+
--- spider/shunqi_spider/shunqi_spider.py
+++ spider/shunqi_spider/shunqi_spider.py
@@ -4,33 +4,39 @@ import csv
 from queue import Queue
 import threading
 import os
+import traceback
 
 """
     顺企网 公司基本信息爬取
     一、获取 顺企网(http://b2b.11467.com/)公司行业分类(大类)(75)
     二、获取 公司行业分类(大类)下每个细分类型的url(1868)
     三、获取 每一个细分类型下的公司url。
-    
+
 """
 
 headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Cookie': 'Hm_lvt_819e30d55b0d1cf6f2c4563aa3c36208=1542459691,1542459766,1542811198,1543747746; Hm_lpvt_819e30d55b0d1cf6f2c4563aa3c36208=1543749436',
-        'Host': 'b2b.11467.com',
-        'Referer': 'http://www.11467.com/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-    }
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Cookie': 'Hm_lvt_819e30d55b0d1cf6f2c4563aa3c36208=1542459691,1542459766,1542811198,1543747746; Hm_lpvt_819e30d55b0d1cf6f2c4563aa3c36208=1543749436',
+    'Host': 'b2b.11467.com',
+    'Referer': 'http://www.11467.com/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+}
 
 detail_category_url_queue = Queue()
 company_url_queue = Queue()
+base_dir = r'E:\code\PythonBasic\spider\shunqi_spider\company'
 
 with open('detail_category.csv', 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
+    i = 0
     for row in reader:
+        i += 1
+        if i < 455:
+            continue
         # print(row)
         detail_category_url_queue.put(row)
-
+print(detail_category_url_queue.qsize())
 
 def get_proxy():
     """
@@ -86,7 +92,7 @@ def get_detail_category_url():
 
             soup = BeautifulSoup(res.text, 'html.parser')
             class_list = soup.select('.boxcontent .listtxt dd a')
-            with open('detail_category.csv', 'a+', encoding='utf-8', newline='') as f :
+            with open('detail_category.csv', 'a+', encoding='utf-8', newline='') as f:
                 writer = csv.writer(f)
                 for item in class_list:
                     print(item)
@@ -107,17 +113,16 @@ def get_company_url():
 
         base_url = detail_category_info[1]  # 行业细分类型url
         category_name = detail_category_info[0]  # 行业细分类型名称
-        base_dir = r'D:\daacheng\Python\PythonBasic\spider\shunqi_spider\company'
-        file_path = os.path.join(base_dir, category_name+'.csv')
+        file_path = os.path.join(base_dir, category_name + '.csv')
+
+        # 获取代理IP
+        proxies = get_proxy()
 
         for i in range(1, 41):
 
             try:
                 url = base_url.replace('.htm', '') + '-' + str(i) + '.htm'
 
-                # 获取代理IP
-                proxies = get_proxy()
-                # print('代理ip：', proxies)
                 if not proxies:
                     res = requests.get(url, headers=headers, timeout=30)
                 else:
@@ -139,21 +144,23 @@ def get_company_url():
                 else:
                     print('返回值: %s, url: %s, 代理IP: %s' % (res.status_code, url, proxies))
             except Exception as e:
+                # print(traceback.format_exc())
                 print('error_url: ', url)
+                with open('error_url.csv', 'a+', encoding='utf-8', newline='') as f_err:
+                    writer = csv.writer(f_err)
+                    writer.writerow([url])
                 continue
 
 
 def main():
-
     td_list = []
-    for i in range(20):
+    for i in range(50):
         td = threading.Thread(target=get_company_url)
         td_list.append(td)
 
     for td in td_list:
         td.start()
     # print(detail_category_url_queue.empty())
-
 
     # get_company_url()
 
